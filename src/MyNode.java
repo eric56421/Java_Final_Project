09a5496621c8b466d341ced71c7583f6631645b1
liang2000 <liang2000.eed07@nctu.edu.tw>
2020-05-16 01:52:19 +0800

    ScrollerPane ok

--- src/MyNode.java
+++ src/MyNode.java
@@ -4,13 +4,14 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.image.*;
 import javafx.scene.control.*;
+import javafx.scene.SnapshotParameters;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class MyNode {
     private ArrayList<MyImage> myImages; // should be replaced by MyWidget
-    private Pane pane;
+    public Pane pane;
     public LinkedList<MyNode> childNodes;
     public MyNode parentNode;
     public ImageView thumbnail;
@@ -24,28 +25,27 @@ public class MyNode {
         myImages = new ArrayList<MyImage>();
         childNodes = new LinkedList<MyNode>();
         flowPane = new FlowPane();
-
         // need to put screenshot here
+        // thumbnail = new ImageView(pane.snapshot(new SnapshotParameters(), null));
         thumbnail = new ImageView("file:../img/beach.jpg");
         thumbnail.setPreserveRatio(true);
-        thumbnail.setFitHeight(flowPane.getPrefWidth());
+        thumbnail.setFitWidth(191);
+        System.out.println("thumbnail width : " + thumbnail.getFitWidth());
     }
 
     public void addChildNode(Pane p) { // need to give a pane with a shape
         childNodes.add(new MyNode(p));
-        try {
-            flowPane.getChildren().addAll(childNodes.get(childNodes.size() - 1).thumbnail);
-            flowPane.setHgap(5);
-            flowPane.setVgap(5);
-            flowPane.setPrefWrapLength(flowPane.getPrefWidth());
-        } catch (Exception e) {
-            System.out.println("MyNode.addChildNode()");
-        }
+        childNodes.getLast().parentNode = this;
+        flowPane.setHgap(5);
+        flowPane.setVgap(5);
+        flowPane.setPrefWrapLength(flowPane.getPrefWidth());
+        flowPane.getChildren().addAll(childNodes.get(childNodes.size() - 1).thumbnail);
         // myIndex.addThumbnail(new Image("beach.jpg"));
     }
 
     public void addMyImage(Image i, double x, double y) {
         myImages.add(new MyImage(i, this.pane));
         myImages.get(myImages.size() - 1).setPosition(x, y);
+        // thumbnail = new ImageView(pane.snapshot(new SnapshotParameters(), null));
     }
 }
--- src/MyPreziSoSexy.java
+++ src/MyPreziSoSexy.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.util.concurrent.CyclicBarrier;
+import java.io.*;
 
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -9,12 +11,13 @@ import javafx.scene.control.SplitPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 
 public class MyPreziSoSexy {
     private MyNode rootMyNode;
 
     // need to add a function that switch currMyNode;
-    private MyNode currMyNode, prevMyNode;
+    private MyNode currMyNode;
 
     public BorderPane middleBorderPane, rightBorderPane;
     public ScrollPane scrollPane;
@@ -30,25 +33,48 @@ public class MyPreziSoSexy {
         scrollPane.setContent(vBox);
         rootMyNode = new MyNode(p);
         currMyNode = rootMyNode;
+        currMyNode.flowPane.setPrefWidth(scrollPane.getPrefWidth());
+        System.out.println("flowPane prefWidth : " + currMyNode.flowPane.getPrefWidth());
+        vBox.getChildren().addAll(currMyNode.flowPane);
     }
 
     public void addChildMyNode() {
         Pane p = new Pane(); // this p should have some default widgets on it.
+        p.setOnDragDropped(event -> {
+            List<File> files = event.getDragboard().getFiles();
+            for (File f : files) {
+                try {
+                    currMyNode.addMyImage(new Image(new FileInputStream(f)), event.getX(), event.getY());
+                } catch (Exception e) {
+                    System.out.println("Can't load image.");
+                }
+            }
+        });
+
+        p.setOnDragOver(event -> {
+            if (event.getDragboard().hasFiles()) {
+                event.acceptTransferModes(TransferMode.ANY);
+            }
+        });
+
         currMyNode.addChildNode(p);
         currMyNode.childNodes.getLast().thumbnail.setOnMouseClicked(event -> gotoMyChildNode(event));
-        vBox.getChildren().addAll(currMyNode.flowPane);
+        currMyNode.childNodes.getLast().flowPane.setMaxWidth(scrollPane.getPrefWidth());
         scrollPane.setContent(vBox);
-        System.out.println("Current node : " + currMyNode);
+        System.out.println("Current node : " + currMyNode + ", Child node : " + currMyNode.childNodes.getLast());
     }
 
     public void gotoMyChildNode(MouseEvent me) {
         for (MyNode n : currMyNode.childNodes) {
             if (n.thumbnail == me.getSource()) {
-                vBox.getChildren().remove(0);
-                prevMyNode = currMyNode;
                 currMyNode = n;
+
+                vBox.getChildren().remove(0);
                 vBox.getChildren().addAll(currMyNode.flowPane);
+                middleBorderPane.getChildren().remove(0);
+                middleBorderPane.setCenter(currMyNode.pane);
                 scrollPane.setContent(vBox);
+
                 System.out.println("Child node found.");
                 break;
             }
@@ -57,9 +83,13 @@ public class MyPreziSoSexy {
     }
 
     public void gotoMyParentNode() {
-        vBox.getChildren().remove(0);
-        vBox.getChildren().addAll(prevMyNode.flowPane);
-        currMyNode = prevMyNode;
+        if (isRootMyNode() != true) {
+            vBox.getChildren().remove(0);
+            vBox.getChildren().addAll(currMyNode.parentNode.flowPane);
+            currMyNode = currMyNode.parentNode;
+            middleBorderPane.setCenter(currMyNode.pane);
+            System.out.println("Jump to node : " + currMyNode);
+        }
     }
 
     public boolean isRootMyNode() {
