f7df810bd46c8f0999b5511dfe4593193141a404
liang2000 <liang2000.eed07@nctu.edu.tw>
2020-05-22 19:55:57 +0800

    Add MouseEventHandlers
    On right mouse button drag : move the pane
    On mouse scrolling : zoom in / out

--- src/MyNode.java
+++ src/MyNode.java
@@ -1,7 +1,11 @@
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.image.*;
 import javafx.scene.control.*;
 import javafx.scene.SnapshotParameters;
@@ -15,29 +19,36 @@ public class MyNode {
     public LinkedList<MyNode> childNodes;
     public MyNode parentNode;
     public ImageView thumbnail;
+    
     // public ScrollPane scrollPane;
     public FlowPane flowPane;
 
     public MyNode(Pane p) {
         this.pane = p;
-        // this.scrollPane = sp;
+        pane.setPrefSize(768, 432);
+        // pane.setBackground(new Background(
+        // new BackgroundImage(new Image("file:../img/default_background.jpg"), 0, 0, 0, 1)));
+
+        pane.setBackground(new Background(new BackgroundFill(Color.RED, null, null)));        
 
         myImages = new ArrayList<MyImage>();
         childNodes = new LinkedList<MyNode>();
         flowPane = new FlowPane();
+
         // need to put screenshot here
         // thumbnail = new ImageView(pane.snapshot(new SnapshotParameters(), null));
         thumbnail = new ImageView("file:../img/beach.jpg");
         thumbnail.setPreserveRatio(true);
         thumbnail.setFitWidth(191);
-        System.out.println("thumbnail width : " + thumbnail.getFitWidth());
     }
 
     public void addChildNode(Pane p) { // need to give a pane with a shape
         childNodes.add(new MyNode(p));
         childNodes.getLast().parentNode = this;
+
         flowPane.setHgap(5);
         flowPane.setVgap(5);
+
         flowPane.setPrefWrapLength(flowPane.getPrefWidth());
         flowPane.getChildren().addAll(childNodes.get(childNodes.size() - 1).thumbnail);
         // myIndex.addThumbnail(new Image("beach.jpg"));
--- src/MyPreziSoSexy.java
+++ src/MyPreziSoSexy.java
@@ -6,12 +6,22 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.SplitPane;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
+import javafx.scene.shape.Rectangle;
+
+import javafx.scene.input.DragEvent;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
 public class MyPreziSoSexy {
     private MyNode rootMyNode;
@@ -20,26 +30,129 @@ public class MyPreziSoSexy {
     private MyNode currMyNode;
 
     public BorderPane middleBorderPane, rightBorderPane;
+    // private final Rectangle middleBoRectangleCenterClip = new Rectangle();
     public ScrollPane scrollPane;
     public VBox vBox;
+    public ButtonBar buttonBar;
 
-    // all parametors are come from FXML
-    public MyPreziSoSexy(Pane p, VBox vb, BorderPane bp1, BorderPane bp2, ScrollPane sp) {
+    // all parametors are from FXML
+    public MyPreziSoSexy(VBox vb, BorderPane bp1, BorderPane bp2, ScrollPane sp, ButtonBar bB) {
         this.middleBorderPane = bp1;
         this.rightBorderPane = bp2;
         this.vBox = vb;
         this.scrollPane = sp;
+        this.buttonBar = bB;
 
-        scrollPane.setContent(vBox);
+        Pane p = new Pane();
+        // p.setClip(middleBoRectangleCenterClip);
+        // p.layoutBoundsProperty().addListener((ov, oldValue, newValue) -> {
+        //     middleBoRectangleCenterClip.setWidth(newValue.getWidth());
+        //     middleBoRectangleCenterClip.setHeight(newValue.getHeight());
+        // });
+
+        addPaneAllListeners(p);
         rootMyNode = new MyNode(p);
+        setupRootPane(p);
+        rootMyNode.pane = p;
+
         currMyNode = rootMyNode;
         currMyNode.flowPane.setPrefWidth(scrollPane.getPrefWidth());
-        System.out.println("flowPane prefWidth : " + currMyNode.flowPane.getPrefWidth());
+
         vBox.getChildren().addAll(currMyNode.flowPane);
+        middleBorderPane.setCenter(currMyNode.pane);
+        scrollPane.setContent(vBox);
+    }
+
+    public void setupRootPane(Pane rp) {
+        rp.getChildren().addAll(new ImageView("file:../img/defult_background.jpg"));
+        // rp.getChildren().addAll(new Label("test"));
     }
 
     public void addChildMyNode() {
         Pane p = new Pane(); // this p should have some default widgets on it.
+
+        addPaneAllListeners(p);
+
+        currMyNode.addChildNode(p);
+        currMyNode.childNodes.getLast().thumbnail.setOnMouseClicked(event -> gotoMyChildNode(event));
+        currMyNode.childNodes.getLast().flowPane.setMaxWidth(scrollPane.getPrefWidth());
+
+        scrollPane.setContent(vBox);
+        System.out.println("Current node : " + currMyNode + ", Child node : " + currMyNode.childNodes.getLast());
+    }
+
+    double mouseAnchorX = 0, mouseAnchorY = 0;
+    double translateAnchorX = 0, translateAnchorY = 0;
+    /** Add all event listeners for pane in MyNode */
+    public void addPaneAllListeners(Pane p) {
+        // ****** scroll test ****** */
+        p.setOnScroll(event -> {
+            double delta = 1.2;
+            double scale = p.getScaleY();
+            double oldScale = scale;
+            DoubleProperty myScale = new SimpleDoubleProperty(1.0);
+
+            if (event.getDeltaY() < 0) {
+                scale /= delta;
+            } else {
+                scale *= delta;
+            }
+
+            final double MAX_SCALE = 10.0;
+            final double MIN_SCALE = 0.1;
+
+            if (scale < MIN_SCALE) {
+                scale = MIN_SCALE;
+            } else if (scale > MAX_SCALE) {
+                scale = MAX_SCALE;
+            }
+
+            double f = scale / oldScale - 1;
+            double dx = (event.getX() - (p.getBoundsInParent().getWidth() / 2 + p.getBoundsInParent().getMinX()));
+            double dy = (event.getY() - (p.getBoundsInParent().getHeight() / 2 + p.getBoundsInParent().getMinY()));
+
+            myScale.set(scale);
+
+            p.scaleXProperty().bind(myScale);
+            p.scaleYProperty().bind(myScale);
+
+            // note: pivot value must be untransformed, i. e. without scaling
+            p.setTranslateX(p.getTranslateX() - f * dx);
+            p.setTranslateY(p.getTranslateY() - f * dy);
+
+            event.consume();
+        });
+
+        EventHandler<MouseEvent> pressedaHandler = new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                // right mouse button => panning
+                if (!event.isSecondaryButtonDown())
+                    return;
+
+                mouseAnchorX = event.getSceneX();
+                mouseAnchorY = event.getSceneY();
+
+                translateAnchorX = p.getTranslateX();
+                translateAnchorY = p.getTranslateY();
+            }
+        };
+        p.setOnMousePressed(pressedaHandler);
+
+        EventHandler<MouseEvent> draggedaHandler = new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent event) {
+                // right mouse button => panning
+                if (!event.isSecondaryButtonDown())
+                    return;
+
+                p.setTranslateX(translateAnchorX + event.getSceneX() - mouseAnchorX);
+                p.setTranslateY(translateAnchorY + event.getSceneY() - mouseAnchorY);
+
+                event.consume();
+            }
+        };
+        p.setOnMouseDragged(draggedaHandler);
+
+        // drag image to add
         p.setOnDragDropped(event -> {
             List<File> files = event.getDragboard().getFiles();
             for (File f : files) {
@@ -56,12 +169,6 @@ public class MyPreziSoSexy {
                 event.acceptTransferModes(TransferMode.ANY);
             }
         });
-
-        currMyNode.addChildNode(p);
-        currMyNode.childNodes.getLast().thumbnail.setOnMouseClicked(event -> gotoMyChildNode(event));
-        currMyNode.childNodes.getLast().flowPane.setMaxWidth(scrollPane.getPrefWidth());
-        scrollPane.setContent(vBox);
-        System.out.println("Current node : " + currMyNode + ", Child node : " + currMyNode.childNodes.getLast());
     }
 
     public void gotoMyChildNode(MouseEvent me) {
@@ -71,6 +178,8 @@ public class MyPreziSoSexy {
 
                 vBox.getChildren().remove(0);
                 vBox.getChildren().addAll(currMyNode.flowPane);
+
+                System.out.println("hihhii" + middleBorderPane.getChildren().size());
                 middleBorderPane.getChildren().remove(0);
                 middleBorderPane.setCenter(currMyNode.pane);
                 scrollPane.setContent(vBox);
